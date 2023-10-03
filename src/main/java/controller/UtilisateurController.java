7f9533c9b82e24afc2eed1d2afec61ea7e012540
Mamadou <senemamadou1999@gmail.com>
2023-10-03 13:43:22 +0000

    ajout des annotations pour le web services jakarta et implémentations des methodes du controlleur

--- src/main/java/controller/UtilisateurController.java
+++ src/main/java/controller/UtilisateurController.java
@@ -1,25 +1,60 @@
 package controller;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 import java.util.List;
 
 import bdbeans.Utilisateur;
 import bdbeans.UtilisateurHome;
-
+@Path("utilisateurs")
 public class UtilisateurController {
-	public List<Utilisateur> getAllUser()
+	private UtilisateurHome userHome;
+	public UtilisateurController()
 	{
-		return new UtilisateurHome().findByExample(null);
-		
-	}
-
-	public void createUser(Utilisateur u)
-	{
-		new UtilisateurHome().persist(u);
-	}
-	public void deleteUser(Utilisateur u)
-	{
-		new UtilisateurHome().delete(u);
+		userHome=new UtilisateurHome();
 	}
 	
+	@GET
+	@Produces({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
+	public List<Utilisateur> getUtilisateurs()
+	{
+		return userHome.getAllUser();
+		
+	}
+	@Path("{id}")
+	@GET
+	@Produces({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
+	public Utilisateur getUtiliateur(@PathParam("id") int id)
+	{
+		return userHome.findUserByID(id);
+	}
+
+	@POST
+	@Produces({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
+	public void add(Utilisateur u)
+	{
+		 userHome.persist(u);
+	}
+	@Path("{idUtilisateur}")
+	@POST
+	@Produces({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
+	public void deleteUser(@PathParam("idUtilisateur")int iduser, Utilisateur u)
+	{
+		u=userHome.findUserByID(iduser);
+		userHome.delete(u);
+	}
+	@Path("{id}")
+	@PUT
+	@Produces({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
+	public void update(@PathParam("id") int idUtilisateur, Utilisateur u)
+	{
+		u=userHome.findUserByID(idUtilisateur);
+		userHome.update(u);
+	}
 
 }
